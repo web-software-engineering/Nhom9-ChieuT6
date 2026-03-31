884265c0fef480b74318689a7371577f43e2a5aa
Lam Bao Chau <cheryle2112@gmail.com>
2026-04-01 00:38:54 +0700

    Fix MySQL port number conversion in db.js

--- backend/services/db.js
+++ backend/services/db.js
@@ -2,10 +2,20 @@ import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  connectTimeout: 30000,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default db;
