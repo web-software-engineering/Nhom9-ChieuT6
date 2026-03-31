c2f16cf3f222241a8b61957272d61d8afd34d714
ChouVG <cheryle2112@gmail.com>
2026-04-01 00:14:20 +0700

    update api + sql

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,19 +1,11 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Lỗi kết nối MySQL:", err);
-  } else {
-    console.log("Kết nối MySQL Railway thành công!");
-  }
 });
 
 export default db;
